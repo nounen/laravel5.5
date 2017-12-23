680b2320114f28de8eff98a81c5501dc249d1bb9
lazyou <1394139539@qq.com>
2017-12-23 11:30:40 +0800

    migrate: create tag

---
+++ database/migrations/2017_12_23_032737_create_tag.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTag extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('sort')->default(0);
+            $table->integer('article_count')->default(0)->comment('关联文章数量');
+            $table->integer('user_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tag');
+    }
+}

47f9adf56212498f3c95506eba4cbe7afe79f3af
linxiaolong <1394139539@qq.com>
2017-12-26 15:46:42 +0800

    删除模态框封装

--- resources/views/layouts/common_table.blade.php
+++ resources/views/layouts/common_table.blade.php
@@ -55,31 +55,7 @@
                                     删除
                                 </button>
 
-                                <div class="modal modal-danger fade" id="delete_modal_{{ $item->id }}">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                    <span aria-hidden="true">&times;</span>
-                                                </button>
-
-                                                <h4 class="modal-title">危险操作</h4>
-                                            </div>
-                                            <div class="modal-body text-center">
-                                                <p>删除后数据不可恢复!</p>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-flat btn-outline pull-left" data-dismiss="modal">取消</button>
-
-                                                <form action="{{ $base_url }}/{{ $item->id }}" method="POST" style="display: inline;">
-                                                    {{ method_field('DELETE') }}
-                                                    {{ csrf_field() }}
-                                                    <button type="submit" class="btn btn-flat btn-danger btn-outline">删除</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                @include('layouts.common_delete_modal', ['id' => $item->id, 'url' => "{$base_url}/{$item->id}"])
                                 <!-- /.modal-content -->
                                 @endif
                             </td>
